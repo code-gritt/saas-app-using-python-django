700b3c8434d4ab2ee5c35173808370a955d350b4
Gokul <gokulchandan24@gmail.com>
2025-06-13 16:36:58 +0530

    created components

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,6 +1,28 @@
-
+import pathlib
+from django.shortcuts import render
 from django.http import HttpResponse
+
+this_dir = pathlib.Path(__file__).resolve().parent
 
 
 def home_page_view(request, *args, **kwargs):
     return HttpResponse("<h1>Gokul learning Django</h1>")
+
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html>
+
+<body>
+    <h1>{page_title} anything?</h1>
+</body>
+</html>    
+""".format(**my_context)  # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
