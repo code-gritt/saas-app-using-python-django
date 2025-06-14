73dc562f8a82c83b6f9d704ad4466078e1ae0d40
Gokul <gokulchandan24@gmail.com>
2025-06-14 15:25:00 +0530

    changes

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,17 +1,27 @@
 import pathlib
 from django.shortcuts import render
 from django.http import HttpResponse
+from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
+def about_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     my_title = "My Page"
-    my_context = {
-        "page_title": my_title
-    }
     html_template = "home.html"
-
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
+    }
+    PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
 
