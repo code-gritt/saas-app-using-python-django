761c2b7cca09b99fef945b09d521f41412fce3c8
Gokul <gokulchandan24@gmail.com>
2025-06-15 14:54:01 +0530

    changes

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,22 +6,17 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def about_view(request, *args, **kwargs):
-    qs = PageVisit.objects.all()
-    page_qs = PageVisit.objects.filter(path=request.path)
-    try:
-        percent = (page_qs.count() * 100.0) / qs.count()
-    except:
-        percent = 0
+def home_page_view(request, *args, **kwargs):
+    queryset = PageVisit.objects.all()
     my_title = "My Page"
     html_template = "home.html"
     my_context = {
         "page_title": my_title,
-        "page_visit_count": page_qs.count(),
-        "percent": percent,
-        "total_visit_count": qs.count(),
+        "queryset":queryset
+
+
     }
-    PageVisit.objects.create(path=request.path)
+    PageVisit.objects.create()
     return render(request, html_template, my_context)
 
 
